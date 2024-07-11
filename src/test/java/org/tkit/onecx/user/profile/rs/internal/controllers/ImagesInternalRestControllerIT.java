7191bd6b041c6e922d6a678d598fdab58c946f07
JordenReuter <149687553+JordenReuter@users.noreply.github.com>
2024-07-11 14:03:54 +0200

    feat: removed image table + added openapi docs (#48)

--- src/test/java/org/tkit/onecx/user/profile/rs/internal/controllers/ImagesInternalRestControllerIT.java
+++
@@ -1,8 +0,0 @@
-package org.tkit.onecx.user.profile.rs.internal.controllers;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-public class ImagesInternalRestControllerIT extends ImagesInternalRestControllerTest {
-
-}
