91a2f0ceef63dbdf7fee9735990d19da18f2ba98
Jorden_Reuter <149687553+JordenReuter@users.noreply.github.com>
2025-06-18 12:35:07 +0200

    feat: added settings to v1 api

--- src/main/java/org/tkit/onecx/user/profile/rs/external/v1/mappers/UserProfileV1Mapper.java
+++ src/main/java/org/tkit/onecx/user/profile/rs/external/v1/mappers/UserProfileV1Mapper.java
@@ -1,8 +1,16 @@
 package org.tkit.onecx.user.profile.rs.external.v1.mappers;
 
+import java.util.Map;
+
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 import org.tkit.onecx.user.profile.domain.models.*;
+import org.tkit.onecx.user.profile.rs.internal.mappers.UserProfileMapper;
 import org.tkit.quarkus.rs.mappers.OffsetDateTimeMapper;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import gen.org.tkit.onecx.user.profile.rs.external.v1.model.*;
 
@@ -17,6 +25,23 @@ public interface UserProfileV1Mapper {
 
     UserPersonPhoneDTO map(UserPersonPhone entity);
 
+    @Mapping(target = "removeSettingsItem", ignore = true)
+    @Mapping(target = "settings", source = "settings", qualifiedByName = "s2m")
     UserProfileAccountSettingsDTO map(UserProfileAccountSettings entity);
+
+    @Named("s2m")
+    default Map<String, String> s2m(String value) {
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        if (value == null || value.isBlank()) {
+            return null;
+        }
+        try {
+            return objectMapper.readValue(value, new TypeReference<Map<String, String>>() {
+            });
+        } catch (Exception e) {
+            throw new UserProfileMapper.MapperException("Error reading parameter value", e);
+        }
+    }
 
 }
